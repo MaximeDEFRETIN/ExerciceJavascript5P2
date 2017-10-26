1f31d270b0aed491e2089dbf791b637543a7e162
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-26 16:14:56 +0200

    Exercice 5 : Javascript

---
+++ Javascript.js
@@ -0,0 +1,7 @@
+function img() {
+  document.getElementById('img1')this.src="img/image1_2.jpg";
+  document.getElementById('img2')this.src=img/image2_2.jpg";
+  document.getElementById('img3')this.src=img/image3_2.jpg";
+  document.getElementById('img4')this.src=img/image4_2.jpg";
+  document.getElementById('img5')this.src=img/image5_2.jpg";
+}
