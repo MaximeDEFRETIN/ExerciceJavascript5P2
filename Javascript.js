da65ac30d10e9762ce4507502400164dd66644f0
Maxime DEFRETIN <maxime.defretin@laposte.net>
2017-10-27 10:28:31 +0200

    Correction

--- Javascript.js
+++ Javascript.js
@@ -1,7 +1,4 @@
-function img() {
-  document.getElementById('img1')this.src="img/image1_2.jpg";
-  document.getElementById('img2')this.src=img/image2_2.jpg";
-  document.getElementById('img3')this.src=img/image3_2.jpg";
-  document.getElementById('img4')this.src=img/image4_2.jpg";
-  document.getElementById('img5')this.src=img/image5_2.jpg";
+function changeimage(image) {
+  //this permet de remplacer le getElementById(ID) que nous récupérons avec le paramètre image
+  image.src='img/' + image.id + '_2.jpg';
 }
